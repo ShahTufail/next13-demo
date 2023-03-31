e91c5820be0f9794af0e8a8f13b4b9f9f1ce1c59
Tufail Mehraj <shahtuffey@gmail.com>
2023-03-31 16:14:45 +0530

    routes, links, metadata, error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import styles from './page.module.css'
 
 export default function About() {
+    // throw new Error('Not today!')
     return (
         <div className={styles.main}>
             <h1>
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Link from "next/link"
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1> Awesome Next 13 🔥</h1>
-      <img
-        className={styles.logo}
-        src='https://i.ytimg.com/vi/K_BYvkRvb58/maxresdefault.jpg'>
-          
-      </img>
+    <main>
+      <h1>Hello World!</h1>
+      <Link href="/about">Go to About Page</Link>
     </main>
   )
 }
