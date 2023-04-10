938ab95b53ac9b212cc2961e5c0f8561a781c149
Tufail Mehraj <shahtuffey@gmail.com>
2023-04-10 15:55:17 +0530

    next 03 commit dynamic routes

--- lib/getAllUsers.ts
+++
@@ -1,7 +0,0 @@
-export default async function getAllUsers() {
-    const data = await fetch('https://jsonplaceholder.typicode.com/users')
-
-    if (!data.ok) throw new Error('failed to fetch data')
-
-    return data.json()
-}
